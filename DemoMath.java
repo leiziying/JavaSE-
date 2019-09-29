e461cc4e1f03cdc8846847db8b0a972aa4c323a1
leiziying <3032483701@qq.com>
2019-09-29 21:45:18 +0800

    提交代码

---
+++ DemoMath.java
@@ -0,0 +1,28 @@
+/*
+java.util.Math类是数学相关的工具类，里面提供了大量的静态方法，完成与数学运算相关的操作
+public static  double obs(double  num);获取绝对值
+public static  double ceil(double  num);向上取整
+public  static double floor(double  num);向下取整
+public static long   round(double  num); 四舍五入
+Math.PI代表近似的圆周率常量
+ */
+public class DemoMath {
+    public static void main(String[] args) {
+        //获取绝对值
+        System.out.println(Math.abs(3.14));
+        System.out.println(Math.abs(0));
+        System.out.println(Math.abs(-2.5));
+        //向上取整
+        System.out.println(Math.ceil(3.9));
+        System.out.println(Math.ceil(3.1));
+        System.out.println(Math.ceil(3.0));
+        //向下取整：抹零
+        System.out.println(Math.floor(30.1));
+        System.out.println(Math.floor(30.9));
+        System.out.println(Math.floor(31.0));
+        //四舍五入(不带小数点)
+        System.out.println(Math.round(20.4));
+        System.out.println(Math.round(10.5));
+        System.out.println(Math.round(23.8));
+    }
+}
