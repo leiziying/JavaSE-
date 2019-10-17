3ad45fc7ae5da5048efb7cdccafc2eab26f85d2b
leiziying <3032483701@qq.com>
2019-10-17 21:15:51 +0800

    抛异常

---
+++ Demo02.java
@@ -0,0 +1,19 @@
+public class Demo02 {
+  public  static void throwExceptionMethod(boolean  isThrow){
+      System.out.println("抛异常之前");
+      if(isThrow){
+          throw  new  NullPointerException();
+      }
+      System.out.println("抛异常之后");
+  }
+
+    public static void main(String[] args) {
+        try{
+            throwExceptionMethod(true);
+        }catch(NullPointerException  exc){
+            System.out.println("捕获到异常");
+        }finally {
+            System.out.println("总得来我这一趟");
+        }
+    }
+}
