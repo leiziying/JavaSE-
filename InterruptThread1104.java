7c9b50f26deee434022e830638cad68f5adce0b9
leiziying <3032483701@qq.com>
2019-11-04 10:08:30 +0800

    提交代码

---
+++ InterruptThread1104.java
@@ -0,0 +1,55 @@
+import javax.sound.midi.Soundbank;
+
+public class InterruptThread1104 {
+    /*
+    * interrupt可以通过异常方式通知
+    * */
+    private static class MyThread1 extends  Thread{
+        @Override
+        public void run(){
+            while(true){
+                try{
+                    Thread.sleep(10000000);
+                }catch (InterruptedException e){
+                    System.out.println(interrupted());
+                    break;
+                }
+            }
+            System.out.println("退出");
+        }
+    }
+    private static class MyThread2 extends  Thread{
+        @Override
+        public void run(){
+            while(!isInterrupted()){
+
+            }
+            //通过判断中断状态退出，状态不会被重置
+            System.out.println(isInterrupted());
+            System.out.println("退出");
+        }
+    }
+    private static class MyThread3 extends  Thread{
+        @Override
+        public void run(){
+            while(!Thread.interrupted()){
+
+            }
+            //通过判断中断状态退出，状态被重置
+            System.out.println(isInterrupted());
+            System.out.println("退出");
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread1 t1=new MyThread1();
+        t1.start();
+        t1.interrupt();
+        MyThread2 t2=new MyThread2();
+        t2.start();
+        t2.interrupt();
+        MyThread3 t3=new MyThread3();
+        t3.start();
+        t3.interrupt();
+    }
+}
